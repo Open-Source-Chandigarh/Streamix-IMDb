099f1e977f3e7d077ae181a14a4ebdc02074583b
harshit391 <harshitsingla1761@gmail.com>
2023-04-30 15:19:09 +0530

    code length

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 const video1 = document.getElementById('my-video1');
 const video2 = document.getElementById('my-video2');
 const video3 = document.getElementById('my-video3');
@@ -7,23 +6,18 @@ const video5 = document.getElementById('my-video5');
 const video6 = document.getElementById('my-video6');
 const video7 = document.getElementById('my-video7');
 const video8 = document.getElementById('my-video8');
-
-
         function playVideoWhenVisible(video) {
         const videoRect = video.getBoundingClientRect();
         const isVisible = (
             videoRect.top >= 0 &&
             videoRect.left >= 0 &&
             videoRect.bottom <= window.innerHeight &&
-            videoRect.right <= window.innerWidth
-        );
+            videoRect.right <= window.innerWidth);
         if (isVisible) {
             video.play();
         } else {
             video.pause();
-        }
-        }
-
+        }}
         window.addEventListener('scroll', function() {
             playVideoWhenVisible(video1);
             playVideoWhenVisible(video2);
@@ -32,10 +26,8 @@ const video8 = document.getElementById('my-video8');
             playVideoWhenVisible(video5);
             playVideoWhenVisible(video6);
             playVideoWhenVisible(video7);
-            playVideoWhenVisible(video8);
-            
+            playVideoWhenVisible(video8); 
           });
-          
           window.addEventListener('resize', function() {
             playVideoWhenVisible(video1);
             playVideoWhenVisible(video2);
@@ -46,4 +38,3 @@ const video8 = document.getElementById('my-video8');
             playVideoWhenVisible(video7);
             playVideoWhenVisible(video8);
           })
-

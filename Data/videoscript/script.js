694e9844053415e275f943992a66f34ced2d5311
harshpreet931 <hsanime@gmail.com>
2023-05-13 13:47:17 +0530

    Fade leftRight

--- Data/videoscript/script.js
+++ Data/videoscript/script.js
@@ -38,3 +38,21 @@ const video8 = document.getElementById('my-video8');
             playVideoWhenVisible(video7);
             playVideoWhenVisible(video8);
           })
+
+function reveal() {
+    var reveals = document.querySelectorAll(".reveal");
+          
+    for (var i = 0; i < reveals.length; i++) {
+        var windowHeight = window.innerHeight;
+        var elementTop = reveals[i].getBoundingClientRect().top;
+        var elementVisible = 150;
+          
+        if (elementTop < windowHeight - elementVisible) {
+            reveals[i].classList.add("active");
+        } else {
+            reveals[i].classList.remove("active");
+        }
+        }
+        }
+window.addEventListener("scroll", reveal);
+          
